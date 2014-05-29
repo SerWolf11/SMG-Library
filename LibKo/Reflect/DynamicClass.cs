3dcddd90da1158b0df321d8b202fa4692ef3000d
serwolf <sergiowolf11@yandex.com>
2014-05-29 14:33:15 -0500

    lib2

---
+++ LibKo/Reflect/DynamicClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.CodeDom;
+using System.Dynamic;
+using System.Reflection.Emit;
+using System.Reflection;
+namespace LibKo.Reflect
+{
+    public class DynamicClass
+    {
+        //static void Main(string[] args)
+        //{
+        //    string className = "BlogPost";
+
+        //    var props = new Dictionary<string, Type>() {
+        //        { "Title", typeof(string) },
+        //        { "Text", typeof(string) },
+        //        { "Tags", typeof(string[]) }
+        //    };
+
+        //    createType(className, props);
+        //}
+
+        public DynamicClass()
+        {
+        }
+
+        public static void createType(string name, IDictionary<string, Type> props)
+        {
+            var csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v4.0" } });
+            var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, "Test.Dynamic.dll", false);
+            parameters.GenerateExecutable = false;
+
+            var compileUnit = new CodeCompileUnit();
+            var ns = new CodeNamespace("Dynamic.Dynamic");
+            compileUnit.Namespaces.Add(ns);
+            ns.Imports.Add(new CodeNamespaceImport("System"));
+
+            var classType = new CodeTypeDeclaration(name);
+            classType.Attributes = MemberAttributes.Public;
+            ns.Types.Add(classType);
+
+            foreach (var prop in props)
+            {
+                var fieldName = "_" + prop.Key;
+                var field = new CodeMemberField(prop.Value, fieldName);
+                classType.Members.Add(field);
+
+                var property = new CodeMemberProperty();
+                property.Attributes = MemberAttributes.Public | MemberAttributes.Final;
+                property.Type = new CodeTypeReference(prop.Value);
+                property.Name = prop.Key;
+                property.GetStatements.Add(new CodeMethodReturnStatement(new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), fieldName)));
+                property.SetStatements.Add(new CodeAssignStatement(new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), fieldName), new CodePropertySetValueReferenceExpression()));
+                classType.Members.Add(property);
+            }
+
+            var results = csc.CompileAssemblyFromDom(parameters, compileUnit);
+            results.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error.ErrorText));
+        }
+
+        public static dynamic GetDynamicObject(Dictionary<string, object> properties)
+        {
+            var dynamicObject = new ExpandoObject() as IDictionary<string, Object>;
+            foreach (var property in properties)
+            {
+                dynamicObject.Add(property.Key, property.Value);
+            }
+            return dynamicObject;
+        }
+
+
+        public DynamicClass Add<T>(string key, T value)
+        {
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("DynamicAssembly"), AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("Dynamic.dll");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType(Guid.NewGuid().ToString());
+            typeBuilder.SetParent(this.GetType());
+            PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(key, PropertyAttributes.None, typeof(T), Type.EmptyTypes);
+
+            MethodBuilder getMethodBuilder = typeBuilder.DefineMethod("get_" + key, MethodAttributes.Public, CallingConventions.HasThis, typeof(T), Type.EmptyTypes);
+            ILGenerator getter = getMethodBuilder.GetILGenerator();
+            getter.Emit(OpCodes.Ldarg_0);
+            getter.Emit(OpCodes.Ldstr, key);
+            getter.Emit(OpCodes.Callvirt, typeof(DynamicClass).GetMethod("Get", BindingFlags.Instance | BindingFlags.NonPublic).MakeGenericMethod(typeof(T)));
+            getter.Emit(OpCodes.Ret);
+            propertyBuilder.SetGetMethod(getMethodBuilder);
+
+            Type type = typeBuilder.CreateType();
+
+            DynamicClass child = (DynamicClass)Activator.CreateInstance(type);
+            child.dictionary = this.dictionary;
+            dictionary.Add(key, value);
+            return child;
+        }
+
+        protected T Get<T>(string key)
+        {
+            return (T)dictionary[key];
+        }
+
+        private Dictionary<string, object> dictionary = new Dictionary<string, object>();
+
+    }
+}
---
+++ LibKo/SystemConfiguration/SystemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+
+namespace LibKo.SystemConfiguration
+{
+    public class SystemConfiguration
+    {
+
+        private void Configuration()
+        {
+            System.Configuration.Configuration config =
+                    System.Configuration.ConfigurationManager.OpenMappedExeConfiguration(
+                        new System.Configuration.ExeConfigurationFileMap()
+                        {
+                            ExeConfigFilename = webconfigFile
+                        },
+            System.Configuration.ConfigurationUserLevel.None);
+            string oldValue = config.AppSettings.Settings["SomeKey"].Value;
+            config.AppSettings.Settings["SomeKey"].Value = "NewValue";
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        public string webconfigFile { get; set; }
+    }
+}
