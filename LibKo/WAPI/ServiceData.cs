af62c675833aaf6830820705fc75cd23b68f9080
SerWolf11 <sergiowolf11@yandex.com>
2014-05-30 15:09:01 -0500

    data

---
+++ LibKo/WAPI/ServiceData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace LibKo.WAPI
+{
+    public class ServiceData
+    {
+        private static HttpClient _clientProperties = Settings.ClientProperties();
+
+        public static HttpClient ClientProperties
+        {
+            get { return ServiceData._clientProperties; }
+            set { ServiceData._clientProperties = value; }
+        }
+
+
+    }
+}
